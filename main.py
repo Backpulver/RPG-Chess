b39a56b27182590155fe4fca5be299924e94c7cd
Yoan Hristov <32713575+Backpulver@users.noreply.github.com>
2023-02-18 05:36:56 +0200

    Added functionality for ending a game

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
     running = True
     square_selected = () #no selected square -> tuple (row, col)
     player_clicks = [] #keep track of player clicks with 2 tuples for ex.  [(6, 4), (4, 4)] -> select and unselect a piece
+    game_over = False
 
     while running:
         for event in pygame.event.get():
@@ -29,28 +30,29 @@ def main():
 
             # mouse handler
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                location = pygame.mouse.get_pos() # (x, y) location of mouse
-                col = location[0] // SQUARE_SIZE
-                row= location[1] // SQUARE_SIZE
-                if square_selected == (row, col): #same square clicked twice = illegal move
-                    square_selected = () #unselect
-                    player_clicks = [] #reset player clicks
-                else:
-                    square_selected = (row, col)
-                    player_clicks.append(square_selected)
+                if not game_over:
+                    location = pygame.mouse.get_pos() # (x, y) location of mouse
+                    col = location[0] // SQUARE_SIZE
+                    row= location[1] // SQUARE_SIZE
+                    if square_selected == (row, col): #same square clicked twice = illegal move
+                        square_selected = () #unselect
+                        player_clicks = [] #reset player clicks
+                    else:
+                        square_selected = (row, col)
+                        player_clicks.append(square_selected)
 
-                if len(player_clicks) == 2: # we have 2 legal clicks
-                    move = chess_engine.Move(player_clicks[0], player_clicks[1], gs.board)
-                    print(move.get_chess_notation())
+                    if len(player_clicks) == 2: # we have 2 legal clicks
+                        move = chess_engine.Move(player_clicks[0], player_clicks[1], gs.board)
+                        print(move.get_chess_notation())
 
-                    for i in range(len(valid_moves)):
-                        if move == valid_moves[i]:
-                            gs.make_move(valid_moves[i])
-                            move_made = True
-                            square_selected = () # move made unselect clicks
-                            player_clicks = []
-                    if not move_made: # fix for clicks if invalid move (we used to click 2 times)
-                        player_clicks = [square_selected]
+                        for i in range(len(valid_moves)):
+                            if move == valid_moves[i]:
+                                gs.make_move(valid_moves[i])
+                                move_made = True
+                                square_selected = () # move made unselect clicks
+                                player_clicks = []
+                        if not move_made: # fix for clicks if invalid move (we used to click 2 times)
+                            player_clicks = [square_selected]
 
             # key handler
             elif event.type == pygame.KEYDOWN:
@@ -69,6 +71,17 @@ def main():
                 move_made = False
 
         draw_game_state(screen, gs, valid_moves, square_selected)
+
+        if gs.checkmate:
+            game_over = True
+            if gs.white_to_move:
+                draw_text(screen, "Black wins by checkmate!")
+            else:
+                draw_text(screen, "White wins by checkmate!")
+        elif gs.stalemate:
+            game_over = True
+            draw_text(screen, "Draw")
+
         clock.tick(FPS)
         pygame.display.flip()
 
@@ -107,7 +120,12 @@ def draw_pieces(screen, board):
             piece = board[row][col]
             if piece != "--":
                 screen.blit(IMAGES[piece], pygame.Rect(col*SQUARE_SIZE, row*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                
+
+def draw_text(screen, text):
+    font = pygame.font.SysFont("Arial", 32, True, False)
+    text_obj = font.render(text, 0, pygame.Color(BLACK))
+    text_location = pygame.Rect(0, 0, WIDTH, HEIGHT).move(WIDTH / 4.2 - text_obj.get_width() / 4.2, HEIGHT / 2 - text_obj.get_height() / 2)
+    screen.blit(text_obj, text_location)
 
 if __name__ == "__main__":
     main()
